6715fb28a26ae31c0f3a196347890fad5b4012ed
danyel117 <dsaldarriaga@prevalentware.com>
2020-11-26 12:48:33 -0500

    agregar animacion y efecto para categorias flotantes

--- components/ListOfCategories/ListOfCategories.js
+++ components/ListOfCategories/ListOfCategories.js
@@ -2,25 +2,48 @@ import React, { useState, useEffect } from 'react';
 import Category from '@components/Category/Category';
 import { List, Item } from './styles';
 import { getCategorias } from '@utils/api';
+import { render } from 'react-dom';
 
 const ListOfCategories = () => {
   const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
 
   useEffect(() => {
     getCategorias().then((res) => {
       setCategories(res);
     });
   }, []);
+
+  useEffect(() => {
+    const onScroll = (e) => {
+      const newShowFixed = window.scrollY > 200;
+      newShowFixed !== showFixed && setShowFixed(newShowFixed);
+    };
+    document.addEventListener('scroll', onScroll);
+    return () => {
+      document.removeEventListener('scroll', onScroll);
+    };
+  }, [showFixed]);
+
+  const renderList = (fixed) => {
+    return (
+      <List className={fixed ? 'fixed' : ''}>
+        {categories.map((category, index) => {
+          return (
+            <Item key={index}>
+              <Category {...category} />
+            </Item>
+          );
+        })}
+      </List>
+    );
+  };
+
   return (
-    <List>
-      {categories.map((category, index) => {
-        return (
-          <Item key={index}>
-            <Category {...category} />
-          </Item>
-        );
-      })}
-    </List>
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   );
 };
 
