24b3d17f8f1cea24d253c15a326e12277716360e
danyel117 <dsaldarriaga@prevalentware.com>
2020-11-26 18:11:44 -0500

    agregar soporte de intersection observer para otros navegadores

--- components/PhotoCard/PhotoCard.js
+++ components/PhotoCard/PhotoCard.js
@@ -10,14 +10,18 @@ const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const ref = useRef(null);
   const [show, setShow] = useState(false);
   useEffect(() => {
-    const observer = new window.IntersectionObserver((entries) => {
-      const { isIntersecting } = entries[0];
-      if (isIntersecting) {
-        setShow(true);
-        observer.disconnect();
-      }
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined' ? window.IntersectionObserver : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver((entries) => {
+        const { isIntersecting } = entries[0];
+        if (isIntersecting) {
+          setShow(true);
+          observer.disconnect();
+        }
+      });
+      observer.observe(ref.current);
     });
-    observer.observe(ref.current);
   }, [ref]);
 
   return (
