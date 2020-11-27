074e514ef2dbd8507b76caa00267fee7fc85b274
danyel117 <dsaldarriaga@prevalentware.com>
2020-11-26 21:20:15 -0500

    agregar comunicación con graphql para fotos

--- components/ListOfPhotoCards/ListOfPhotoCards.js
+++ components/ListOfPhotoCards/ListOfPhotoCards.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PhotoCard from '@components/PhotoCard/PhotoCard';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
 const ListOfPhotoCards = () => {
+  const { loading, error, data } = useQuery(gql`
+    query getPhotos {
+      photos {
+        id
+        categoryId
+        src
+        likes
+        userId
+        liked
+      }
+    }
+  `);
+
+  useEffect(() => {
+    console.log(loading, error, data);
+  }, [loading, error, data]);
+
   return (
     <ul>
-      {[1, 2, 3].map((id) => {
-        return <PhotoCard key={id} id={id} />;
-      })}
+      {data &&
+        data.photos.map((photo) => {
+          return <PhotoCard key={photo.id} {...photo} />;
+        })}
     </ul>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,26 @@
 import { GlobalStyle } from './GlobalStyles';
 import Logo from '@components/Logo/Logo';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const cache = new InMemoryCache();
+
+const link = new HttpLink({
+  uri: 'https://petgram-server-24iykciv5.now.sh/graphql',
+});
+
+const client = new ApolloClient({
+  link: link,
+  cache: cache,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ApolloProvider client={client}>
       <GlobalStyle />
       <Logo />
       <Component {...pageProps} />
-    </>
+    </ApolloProvider>
   );
 }
 
