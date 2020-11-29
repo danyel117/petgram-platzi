da11ca3878c95a7d3f0dffd6927b8831a31a45b9
danyel117 <dsaldarriaga@prevalentware.com>
2020-11-29 14:49:28 -0500

    agregar middleware de autenticación con Apollo Link

--- components/PhotoCard/PhotoCard.js
+++ components/PhotoCard/PhotoCard.js
@@ -2,14 +2,13 @@ import React, { useEffect, useReducer, useRef, useState } from 'react';
 import Link from 'next/link';
 import { ImgWrapper, Img, Article } from './styles';
 import useIntersectionObserver from 'hooks/useIntersectionObserver';
-import useLocalStorage from 'hooks/useLocalStorage';
 import FavButton from 'components/FavButton/FavButton';
 import { gql } from 'apollo-boost';
 import { useMutation } from '@apollo/react-hooks';
 
 const MUTATION_LIKE_PHOTO = gql`
-  mutation likeAnonymousPhoto($input: LikePhoto!) {
-    likeAnonymousPhoto(input: $input) {
+  mutation likePhoto($input: LikePhoto!) {
+    likePhoto(input: $input) {
       id
       liked
       likes
@@ -20,15 +19,11 @@ const MUTATION_LIKE_PHOTO = gql`
 const DEFAULT_IMAGE =
   'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
 
-const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const ref = useRef(null);
   const { show } = useIntersectionObserver(ref);
-  const key = `like-${id}`;
-  const [liked, setLiked] = useLocalStorage(key, false);
   const [mutation, { loading: mutationLoading, error: mutationError }] = useMutation(MUTATION_LIKE_PHOTO);
   const toggleLike = () => {
-    setLiked(!liked);
-    !liked &&
       mutation({
         variables: {
           input: { id },
--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,40 @@ import React,{useState} from 'react'
 import { GlobalStyle } from './GlobalStyles';
 import Logo from '@components/Logo/Logo';
 import NavBar from '@components/NavBar/NavBar'
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { ApolloClient, InMemoryCache, ApolloLink, from, HttpLink } from '@apollo/client';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { onError } from '@apollo/client/link/error';
+import { setContext } from '@apollo/client/link/context';
 import {AuthContext} from 'context/auth'
-const cache = new InMemoryCache();
 
-const link = new HttpLink({
-  uri: 'https://petgram-server-24iykciv5.now.sh/graphql',
-});
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = window.localStorage.getItem('token')
+  if (token) {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+  }
+  return forward(operation)
+})
+const errorMiddleware = onError(({ networkError }) => {
+  if (networkError && networkError.result.code === 'invalid_token') {
+    window.localStorage.removeItem('token')
+    window.location = '/user'
+  }
+})
 
 const client = new ApolloClient({
-  link: link,
-  cache: cache,
-});
-
+  cache: new InMemoryCache(),
+  link: from([
+    errorMiddleware,
+    authMiddleware,
+    new HttpLink({
+      uri: 'https://petgram-server-24iykciv5.now.sh/graphql'
+    })
+  ])
+})
 function MyApp({ Component, pageProps }) {
   const [authTokens,setAuthTokens] = useState(()=>{
     try{
